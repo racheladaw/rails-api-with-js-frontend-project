513aad247fb8275d9a5fe10d750ac05b959e89d9
Rachel <williamsrachelada@gmail.com>
2019-10-21 12:30:39 -0700

    Reorganized js code into classes

--- quick-recipe-frontend/src/index.js
+++ quick-recipe-frontend/src/index.js
@@ -30,7 +30,7 @@ class Recipe {
     ingHeader.innerHTML = "Ingredients:"
     cardInfo.appendChild(ingHeader)
     const ul = document.createElement('ul')
-    for (ingredient of this.ingredients) {
+    for (let ingredient of this.ingredients) {
       let li = document.createElement('li')
       li.innerHTML = ingredient
       ul.appendChild(li)
@@ -44,36 +44,101 @@ class Recipe {
     cardContainer.appendChild(card)
   }
 
-}
+  static getRecipes() {
+    fetch(RECIPES_URL).then(response => response.json()).then(json => Recipe.createRecipes(json.data))
+  }
 
-function getRecipes() {
-  fetch(RECIPES_URL).then(response => response.json()).then(json => createRecipes(json.data))
-}
+  static createRecipes(recipes) {
+    const recipeArray = []
+    for (let recipe of recipes) {
+      let ingredientArray = [];
+      for (let ingredient of recipe.attributes.ingredients) {
+        ingredientArray.push(ingredient.name)
+      }
+      recipeArray.push(new Recipe(recipe.attributes.title, recipe.attributes.image_link, recipe.attributes.recipe_link, ingredientArray))
+    }
+    return Recipe.addRecipesToDom(recipeArray)
+  }
 
-function createRecipes(recipes) {
-  const recipeArray = []
-  for (recipe of recipes) {
+  static addRecipesToDom(recipeArray) {
+    for (let recipe of recipeArray) {
+      recipe.createRecipeCard()
+    }
+  }
+
+  static addRecipe() {
+    const form = event.target.parentElement
+    const ingredients = form[3].value.split(', ')
+    const recipe = new Recipe(form[0].value, form[1].value, form[2].value, ingredients)
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        "title": form[0].value,
+        "image_link": form[1].value,
+        "recipe_link": form[2].value,
+        "ingredients": ingredients
+      })
+    };
+    fetch(RECIPES_URL, configurationObject)
+      .then(response => response.json())
+      .then(function(json) {
+        recipe.createRecipeCard();
+        toggleButtons();
+        toggleForm();
+      })
+      .catch(error => console.log("Error: " + error))
+
+  }
+
+  static clearRecipes() {
+    cardContainer.innerHTML = ""
+  }
+
+  static getRandomRecipeByIngredient() {
+    Recipe.clearRecipes();
+    const ingredient = event.target.value
+    fetch(RECIPES_URL + `/${ingredient}`).then(response => response.json()).then(json => Recipe.loadRandomRecipe(json.data.attributes))
+  }
+
+  static loadRandomRecipe(recipe) {
     let ingredientArray = [];
-    for (ingredient of recipe.attributes.ingredients) {
+    for (let ingredient of recipe.ingredients) {
       ingredientArray.push(ingredient.name)
     }
-    recipeArray.push(new Recipe(recipe.attributes.title, recipe.attributes.image_link, recipe.attributes.recipe_link, ingredientArray))
+    const r = new Recipe(recipe.title, recipe.image_link, recipe.recipe_link, ingredientArray)
+    r.createRecipeCard();
   }
-  return addRecipesToDom(recipeArray)
+
 }
 
-function addRecipesToDom(recipeArray) {
-  for (recipe of recipeArray) {
-    recipe.createRecipeCard()
+class Ingredient {
+
+  static getIngredients() {
+    fetch(INGREDIENTS_URL).then(response => response.json()).then(json => Ingredient.populateIngredientDropDown(json.data))
   }
+
+  static populateIngredientDropDown(data) {
+    data.sort((a, b) => (a.attributes.name > b.attributes.name) ? 1 : -1)
+    for (let ingredient of data) {
+      let option = document.createElement("option")
+      option.value = ingredient.attributes.name
+      option.innerHTML = ingredient.attributes.name
+      ingredientDropDown.appendChild(option)
+    }
+  }
+
 }
 
 document.addEventListener("DOMContentLoaded", function() {
   console.log("DOMloaded")
-  getRecipes();
+  Recipe.getRecipes();
   formSubmit.addEventListener("click", function() {
     event.preventDefault();
-    addRecipe();
+    Recipe.addRecipe();
   })
   addRecipeButton.addEventListener("click", function() {
     toggleForm();
@@ -84,7 +149,7 @@ document.addEventListener("DOMContentLoaded", function() {
     toggleButtons();
   })
   ingredientDropDown.addEventListener("change", function() {
-    getRandomRecipeByIngredient();
+    Recipe.getRandomRecipeByIngredient();
   })
 })
 
@@ -97,10 +162,6 @@ function toggleForm() {
   }
 }
 
-function clearRecipes() {
-  cardContainer.innerHTML = ""
-}
-
 function toggleDropDown() {
   const dropDown = document.getElementById("filter-drop-down")
   if (dropDown.classList.contains("hidden")) {
@@ -108,21 +169,7 @@ function toggleDropDown() {
   } else {
     dropDown.className += " hidden"
   }
-  getIngredients();
-}
-
-function getIngredients() {
-  fetch(INGREDIENTS_URL).then(response => response.json()).then(json => populateIngredientDropDown(json.data))
-}
-
-function populateIngredientDropDown(data) {
-  data.sort((a, b) => (a.attributes.name > b.attributes.name) ? 1 : -1)
-  for (ingredient of data) {
-    let option = document.createElement("option")
-    option.value = ingredient.attributes.name
-    option.innerHTML = ingredient.attributes.name
-    ingredientDropDown.appendChild(option)
-  }
+  Ingredient.getIngredients();
 }
 
 function toggleButtons() {
@@ -131,48 +178,4 @@ function toggleButtons() {
   } else {
     formButtons.className += " hidden";
   }
-}
-
-
-function addRecipe() {
-  const form = event.target.parentElement
-  const ingredients = form[3].value.split(', ')
-  const recipe = new Recipe(form[0].value, form[1].value, form[2].value, ingredients)
-  const configurationObject = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify({
-      "title": form[0].value,
-      "image_link": form[1].value,
-      "recipe_link": form[2].value,
-      "ingredients": ingredients
-    })
-  };
-  fetch(RECIPES_URL, configurationObject)
-    .then(response => response.json())
-    .then(function(json) {
-      recipe.createRecipeCard();
-      toggleButtons();
-      toggleForm();
-    })
-    .catch(error => console.log("Error: " + error))
-
-}
-
-function getRandomRecipeByIngredient() {
-  clearRecipes();
-  const ingredient = event.target.value
-  fetch(RECIPES_URL + `/${ingredient}`).then(response => response.json()).then(json => loadRandomRecipe(json.data.attributes))
-}
-
-function loadRandomRecipe(recipe) {
-  let ingredientArray = [];
-  for (ingredient of recipe.ingredients) {
-    ingredientArray.push(ingredient.name)
-  }
-  const r = new Recipe(recipe.title, recipe.image_link, recipe.recipe_link, ingredientArray)
-  r.createRecipeCard();
 }
